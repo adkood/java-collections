1cca987190e0371d440f87e7effd98b295749392
adkood <ashutoshnegi195@gmail.com>
2025-12-01 07:38:41 +0530

    streams

---
+++ streams/Test.java
@@ -0,0 +1,15 @@
+import java.util.function.Predicate;
+
+public class Test {
+
+    public static void main(String[] args) {
+        
+        // predicate 
+
+        // Predicate<Integer> isEven = x -> x%2 == 0;
+        // Predicate<Integer> isDivBy10 = x -> x%10 == 0;  
+        // System.out.println(isEven.and(isDivBy10).test(100));
+        
+    }
+
+}
