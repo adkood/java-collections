d6bc5265588914738ac83ff7da74ce43c9ce3d31
adkood <ashutoshnegi195@gmail.com>
2025-12-07 06:54:02 +0530

    vector is threadsafe

--- list/MyList.java
+++ list/MyList.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Vector;
 
-class Student implements Comparable<Student>{
+class Student {
     String name;
     int age;
 
@@ -12,37 +14,76 @@ class Student implements Comparable<Student>{
     }   
 
     @Override
-    public int compareTo(Student o) {
-        return Integer.compare(this.age, o.age);
-    }
-
-    @Override
     public String toString() {
         return "Student{name='" + name + "', age=" + age + "}";
     }
 }
 
-class sortByName implements Comparator<Student>  {
+class Counter {
+    private List<Integer> count = new Vector<>();
 
-    @Override
-    public int compare(Student o1, Student o2) {
-        return o1.name.compareTo(o2.name);
+    public void addElement(int i) {
+        count.add(i);
+    }
+
+    public int getSize() {
+        return count.size();
     }
 }
+
+class task1 implements Runnable {
+
+    Counter counter;
+
+    public task1(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 1000; i++) {
+            counter.addElement(i);
+        }
+    }
+}
+
+class task2 implements Runnable {
+
+    Counter counter;
+
+    public task2(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 1000; i++) {
+            counter.addElement(i);
+        }
+    }
+}
+
 
 public class MyList {
     
     public static void main(String[] args) {
         
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(new task1(counter));
+        Thread t2 = new Thread(new task2(counter));
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (Exception e) {
+            e.printStackTrace();   
+        }
         
-        List<Student> l = new ArrayList<>();
-        l.add(new Student("ashu", 25));
-        l.add(new Student("rahul", 22));
-        l.add(new Student("zoya", 23));
-
-        l.sort(new sortByName());
-
-        System.out.println(l);
+        System.out.println(counter.getSize());
     }
 
 }
