9849ca84f250667550f97fb4f69cf903a5d42048
adkood <ashutoshnegi195@gmail.com>
2025-12-07 16:13:04 +0530

    queues completed

--- queue/MyQueue.java
+++ queue/MyQueue.java
@@ -4,19 +4,15 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.SynchronousQueue;
 
 public class MyQueue {
- 
+
     public static void main(String[] args) {
-        
-        Deque<Integer> q = new ArrayDeque<>();
-
-        q.add(13);
-        q.addLast(79);
-
-        q.add(100);
-
-        System.out.println(q);
     }
-    
+
 }
