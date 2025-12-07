c021fe92b0b962f3dea097738db44abd3be7bb1e
adkood <ashutoshnegi195@gmail.com>
2025-12-07 13:28:46 +0530

    hahsmap vs weakhashmap

--- map/MyMap.java
+++ map/MyMap.java
@@ -1,20 +1,40 @@
 package map;
 
+import java.lang.ref.WeakReference;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.WeakHashMap;
+
+class Car {
+    String name;
+
+    Car(String name) {
+        this.name = name;
+    }
+
+    public void start() {
+        System.out.println("vroom vroom");
+    }
+
+    @Override
+    public String toString() {
+        return "{ name : " + name + " }";
+    }
+}
 
 public class MyMap {
 
     public static void main(String[] args) {
         
-        // key -> value -> bucket -> hashFunc
+       Map<Car, Integer> mp = new HashMap<>();
 
-        Map<String, Integer> mp = new HashMap<>();
-
-        mp.put(null, null);
-        mp.put("banana", 12);
-
-        System.out.println(mp);
+       Car c1 = new Car("swift");
+       mp.put(c1, 23);
+       
+       c1 = null;
+        System.gc();
+       System.out.println(mp);
 
     }
 
