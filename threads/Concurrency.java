7f1dd3029231e46556e7a33ab5baa4ba11ddddf1
adkood <ashutoshnegi195@gmail.com>
2025-12-10 19:03:43 +0530

    ways to create threads

--- threads/Concurrency.java
+++ threads/Concurrency.java
@@ -1,5 +1,19 @@
 package threads;
 
+
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("inside runnable");
+    }
+}
+
 public class Concurrency {
-    
+
+    public static void main(String[] args) {
+        Thread th = new Thread(new MyRunnable());
+        th.start();
+
+    }
+
 }
