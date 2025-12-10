ec7ce43cf94c00a35e0748cc55986402b943e26f
adkood <ashutoshnegi195@gmail.com>
2025-12-10 19:35:19 +0530

    thread life cycle

--- threads/Concurrency.java
+++ threads/Concurrency.java
@@ -1,18 +1,36 @@
 package threads;
 
 
-class MyRunnable implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("inside runnable");
-    }
-}
-
 public class Concurrency {
 
     public static void main(String[] args) {
-        Thread th = new Thread(new MyRunnable());
+
+        Thread th = new Thread(() -> {
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("inside thread");
+        });
+
+        System.out.println(th.getState());
+
         th.start();
+
+        System.out.println(th.getState());
+
+        try {
+
+            Thread.sleep(2000);
+            System.out.println(th.getState());
+            th.join();
+        } catch (InterruptedException e) {
+            th.interrupt();
+        }
+
+        System.out.println(th.getState());
 
     }
 
