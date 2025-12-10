462e2dc733a19725b11238f2c4c580967eaaa87b
adkood <ashutoshnegi195@gmail.com>
2025-12-10 19:51:56 +0530

    Thread methods + Deamon threads

--- threads/Concurrency.java
+++ threads/Concurrency.java
@@ -1,5 +1,22 @@
 package threads;
 
+class MyDeamon extends Thread {
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.currentThread().sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("Deamon : " + i);
+        }
+
+    }
+
+}
 
 public class Concurrency {
 
@@ -12,25 +29,32 @@ public class Concurrency {
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
-            System.out.println("inside thread");
+            System.out.println("thread 1");
         });
 
-        System.out.println(th.getState());
+        Thread th2 = new Thread(() -> {
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("thread 2");
+        });
+
+        Thread th3 = new MyDeamon();
+        th3.setDaemon(true);
 
         th.start();
-
-        System.out.println(th.getState());
+        th2.start();
+        th3.start();
 
         try {
-
-            Thread.sleep(2000);
-            System.out.println(th.getState());
             th.join();
+            th2.join();
         } catch (InterruptedException e) {
-            th.interrupt();
+            Thread.currentThread().interrupt();
         }
-
-        System.out.println(th.getState());
 
     }
 
