8e4312a3243b5c4f1b965227674fcaf3fceb6e3b
adkood <ashutoshnegi195@gmail.com>
2025-12-11 07:40:04 +0530

    completable future

--- threads/Concurrency.java
+++ threads/Concurrency.java
@@ -1,99 +1,42 @@
 package threads;
 
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
-class MyRunnable implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("Inside runnable, no return, does not throw exception");
-    }
-}
-
-class MyCallable1 implements Callable<String> {
-
-    private CyclicBarrier barrier;
-
-    MyCallable1(CyclicBarrier barrier) {
-        this.barrier = barrier;
-    }
-
-    @Override
-    public String call() throws Exception {
-        // try {
-        Thread.sleep(1000);
-        barrier.await();
-        return "hello from callable 1";
-
-        // } finally {
-
-        // }
-    }
-}
-
-class MyCallable2 implements Callable<String> {
-
-    private CyclicBarrier barrier;
-
-    MyCallable2(CyclicBarrier barrier) {
-        this.barrier = barrier;
-    }
-
-    @Override
-    public String call() throws Exception {
-        try {
-            Thread.sleep(2000);
-            barrier.await();
-            return "hello from callable 2";
-        } finally {
-            // latch.countDown();
-        }
-    }
-}
-
-class MyCallable3 implements Callable<String> {
-
-    private CyclicBarrier barrier;
-
-    MyCallable3(CyclicBarrier barrier) {
-        this.barrier = barrier;
-    }
-
-    @Override
-    public String call() throws Exception {
-        try {
-            Thread.sleep(3000);
-            barrier.await();
-
-            return "hello from callable 3";
-        } finally {
-            // latch.countDown();
-        }
-    }
-}
 
 public class Concurrency {
 
+    public static CompletableFuture<String> getUserId() {
+        return CompletableFuture.supplyAsync(() -> "userId");
+    }
+
+    public static CompletableFuture<Integer> getOrders(String userId) throws Exception {
+        if (userId.equals("userId")) {
+            throw new Exception("testing completable future");
+        }
+        return CompletableFuture.supplyAsync(() -> 100);
+    }
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-        ExecutorService eService = Executors.newFixedThreadPool(3);
 
-        int parties = 3;
-        CyclicBarrier barrier = new CyclicBarrier(parties,
-                () -> {
-                    System.out.println("All parties arrived at the barrier. Now start!");
-                    eService.shutdown();
+        CompletableFuture<?> future = getUserId().thenCompose(n -> {
+            try {
+                return getOrders(n);
+            } catch (Exception e) {
+                System.out.println("Custom : " + e);
+            }
+            return null;
+        })
+        .handle((result, ex) -> {
+                    if (ex != null) {
+                        System.out.println("Error handled: " + ex.getMessage());
+                        return 0; // recover with 0
+                    }
+                    return result * 10; // normal flow
                 });
 
-        eService.submit(new MyCallable1(barrier));
-        eService.submit(new MyCallable2(barrier));
-        eService.submit(new MyCallable3(barrier));
+
+        System.out.println(future.get());
 
     }
 
