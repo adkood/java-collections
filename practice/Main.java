cf60ddca34c95666734c2f39dd25a1a803097d53
adkood <ashutoshnegi195@gmail.com>
2025-12-13 17:12:05 +0530

    synchronized key is reenterant

--- practice/Main.java
+++ practice/Main.java
@@ -1,15 +1,22 @@
 
+class Test {
+
+    public synchronized void funcA() {
+        System.out.println("func a");
+    };
+
+    public synchronized void funcB() {
+       System.out.println("func b");
+       funcA(); 
+    };
+}
+
+
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-
-        Thread th = new Thread(() -> {
-            System.out.println("Inside new thread");
-        });
-
-        th.start();
-        th.start();
-
+        Test test = new Test();
+        test.funcB();
     }
 
 }
