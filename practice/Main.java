dd79564f98d3b15b609ec11075324a341370142c
adkood <ashutoshnegi195@gmail.com>
2025-12-13 15:26:47 +0530

    thread basic s done

--- practice/Main.java
+++ practice/Main.java
@@ -1,9 +1,14 @@
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        
+        Thread th = new Thread(() -> {
+            System.out.println("Inside new thread");
+        });
+
+        th.start();
+        th.start();
 
     }
 
