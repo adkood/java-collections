4fa38c684877aa3390395d3f199cfe5c852398b3
adkood <ashutoshnegi195@gmail.com>
2025-12-15 18:58:05 +0530

    completable future tested

--- practice/Main.java
+++ practice/Main.java
@@ -1,29 +1,29 @@
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static CompletableFuture<String> getUserByUserId(String userId) {
+        System.out.println("Fetching user for userId : " + userId);
+        if(userId.equals("101")) throw new RuntimeException("Something went wrong");
+        return CompletableFuture.completedFuture("ashutosh");
+    }
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-        ExecutorService service = Executors.newFixedThreadPool(1);
-
-        // Corrected Test for Scenario 2
-        Future<?> future = service.submit(() -> {
-            System.out.println("Task has started.");
-            Thread.sleep(3000);
-            System.out.println("running...");
-            return 42;
+        CompletableFuture<String> userId = CompletableFuture.supplyAsync(() -> {
+            System.out.println("UserId fetched for user");
+            return "101";
+        });
+        
+        CompletableFuture<String> user = userId.thenCompose(n -> getUserByUserId(n)).handle((res, err) -> {
+            if(res == null) {
+                System.out.println("Something went wrong");
+                return "Default user"; 
+            }
+            return res;
         });
 
-        Thread.sleep(2900); // <-- ADDED: Ensure the task has time to start
-        future.cancel(false); // This should now find the task running
-
-        System.out.println("Future isCancelled: " + future.isCancelled()); // Should print false
-        // ...
-
-        service.shutdown();
+        System.out.println(user.get());
     }
 
 }
