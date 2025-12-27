8d10611cce8d5e53e8ab104ba9777e04509eeefa
adkood <ashutoshnegi195@gmail.com>
2025-12-27 11:04:35 +0530

    Create inmemory storage for cache

--- practice/Main.java
+++ practice/Main.java
@@ -1,6 +1,44 @@
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+interface Storage<K,V> {
+    void set(K key, V value);
+    V get(K key);
+    void remove(K key);
+}
+
+class InMemoryStorage<K,V> implements Storage<K,V> {
+
+    private Map<K,V> store;
+ 
+    InMemoryStorage() {
+        store = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public void set(K key, V value) {
+        store.put(key, value);
+    }
+
+    @Override
+    public V get(K key) {
+        return store.get(key);
+    }
+
+    @Override
+    public void remove(K key) {
+        store.remove(key);
+    }
+}
+
+interface cache {
+
+}
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+
+
 
     }
 
