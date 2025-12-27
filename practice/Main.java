77cde76be53ee7967e4b0f88d2246dafade8dc46
adkood <ashutoshnegi195@gmail.com>
2025-12-27 12:29:21 +0530

    debug issue in LRU cache

--- practice/Main.java
+++ practice/Main.java
@@ -1,16 +1,20 @@
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-interface Storage<K,V> {
+interface Storage<K, V> {
     void set(K key, V value);
+
     V get(K key);
+
     void remove(K key);
+
+    boolean contains(K key);
 }
 
-class InMemoryStorage<K,V> implements Storage<K,V> {
+class InMemoryStorage<K, V> implements Storage<K, V> {
 
-    private Map<K,V> store;
- 
+    private Map<K, V> store;
+
     InMemoryStorage() {
         store = new ConcurrentHashMap<>();
     }
@@ -29,16 +33,152 @@ class InMemoryStorage<K,V> implements Storage<K,V> {
     public void remove(K key) {
         store.remove(key);
     }
+
+    @Override
+    public boolean contains(K key) {
+        return store.containsKey(key);
+    }
 }
 
-interface cache {
+interface Cache<K, V> {
+    void set(K key, V value);
+
+    V get(K key);
+}
+
+class Node<K> {
+
+    Node<K> prev;
+    Node<K> next;
+    K data;
+
+    Node() {
+        this.data = null;
+        this.next = null;
+        this.prev = null;
+    }
+
+    Node(K data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class LruCache<K, V> implements Cache<K, V> {
+
+    private Node<K> head;
+    private Node<K> tail;
+    private Storage<K, V> storage;
+    private int cap;
+    private int size;
+
+    LruCache(int cap) {
+        storage = new InMemoryStorage<>();
+        head = new Node<>();
+        tail = new Node<>();
+        this.cap = cap;
+        this.size = 0;
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    @Override
+    public synchronized void set(K key, V value) {
+
+        if (storage.contains(key)) {
+
+            storage.set(key, value);
+
+            Node<K> nn = head;
+
+            while (nn != tail && nn.data != key) {
+                nn = nn.next;
+            }
+
+            if (nn.prev != null)
+                nn.prev.next = nn.next;
+            if (nn.next != null)
+                nn.next.prev = nn.prev;
+
+            tail.prev.next = nn;
+            nn.prev = tail.prev;
+            nn.next = tail;
+            tail.prev = nn;
+
+        } else {
+
+            if (size == cap) {
+
+                storage.remove(head.data);
+
+                Node<K> newhead = head.next;
+                head.next = null;
+                newhead.prev = null;
+                head = newhead;
+
+            } else {
+
+                storage.set(key, value);
+
+                Node<K> nn = new Node<>(key);
+
+                if (head.data == null) {
+                    head = nn;
+                    nn.next = tail;
+                    tail.prev = nn;
+                } else {
+                    tail.prev.next = nn;
+                    nn.prev = tail.prev;
+                    nn.next = tail;
+                    tail.prev = nn;
+                }
+                size++;
+            }
+
+        }
+    }
+
+    @Override
+    public synchronized V get(K key) {
+
+        Node<K> nn = head;
+
+        while (nn != tail && nn.data != key) {
+            nn = nn.next;
+        }
+
+        if (nn.prev != null)
+            nn.prev.next = nn.next;
+        if (nn.next != null)
+            nn.next.prev = nn.prev;
+
+        tail.prev.next = nn;
+        nn.prev = tail.prev;
+        nn.next = tail;
+        tail.prev = nn;
+
+        return storage.get(key);
+    }
 
 }
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
+        Cache<String, Integer> cache = new LruCache<>(5);
 
+        cache.set("one", 1);
+        cache.set("two", 2);
+        cache.set("three", 3);
+        cache.set("four", 4);
+        cache.set("five", 5);
+
+        // cache.get("one");
+        cache.set("six", 6);
+
+        System.out.println(cache.get("six"));
 
     }
 
